1f2bac4a415f24a9a512b2c04a5494af3d389ce7
liuxiaokang <floweylyu@outlook.com>
2022-09-21 19:56:04 +0800

    增加打开选课界面

--- gui_yunxiao.py
+++ gui_yunxiao.py
@@ -1,7 +1,8 @@
 #!/usr/local/bin/python3
 # -*- coding: UTF-8 -*-
-from cProfile import label
 from threading import Thread
+import threading
+import time
 import wx
 # import yunxiao as yx
 import yunxiao_union as yxu
@@ -38,6 +39,72 @@ class YunxiaoAboutFrame( wx.Frame):
         self.Center()
         self.Show()
 
+class YunxiaoSelectorFrame( wx.Frame):
+    yunxiao = None
+    course = None
+    parent = None
+    index = 0
+
+    list_subject = None
+    button_update = None
+
+    def __init__( self, parent, yunxiao, course, index):
+        super( YunxiaoSelectorFrame, self).__init__( parent, title = "", size = ( 600, 600))
+        self.yunxiao = yunxiao
+        self.course = course
+        self.parent = parent
+        self.index = index
+
+        self.SetTitle( "当前选课：[{0}] | UUID：[{1}]".format( course[0], course[1]))
+        self.CreateStatusBar( 3)
+
+        subject_columns = [ "已选择？", "课程编号", "课程名", "课程信息", "课程ID"]
+        self.list_subject = wx.ListCtrl( self, style = wx.LC_REPORT)
+        for i in range( len( subject_columns)):
+            iw = 60 if i == 0 or i == 1 or i == 2 else 210
+            self.list_subject.InsertColumn( i, subject_columns[i], width = iw)
+        
+        self.button_update = wx.Button( self, label = "更新列表")
+
+        self.Bind( wx.EVT_CLOSE, self.DidCloseFrame, self)
+        self.Bind( wx.EVT_SIZE, self.DidChangeSize, self)
+        self.Bind( wx.EVT_BUTTON, self.DidRefresh, self.button_update)
+        self.list_subject.Fit()
+        self.Show()
+
+        self.UpdateSubjectList()
+    
+    def DidChangeSize( self, event):
+        sb = self.GetStatusBar()
+        r1 = sb.GetFieldRect( 2)
+        p1 = sb.GetPosition()
+        r1.x += p1.x; r1.y += p1.y
+        r1.width -= 5
+        self.button_update.SetRect( r1)
+
+        r2 = self.GetClientRect()
+        self.list_subject.SetRect( r2)
+        pass
+    
+    def DidRefresh( self, event):
+        self.UpdateSubjectList()
+    
+    def DidCloseFrame( self, event):
+        self.parent.DidCloseActivityWindow( self.index)
+        self.Destroy()
+    
+    def UpdateSubjectListSync( self):
+        wx.CallAfter( self.button_update.Enable)
+        if True:
+            wx.CallAfter( self.SetStatusText, "更新完成", 0)
+    
+    def UpdateSubjectList( self):
+        self.SetStatusText( "正在更新课程列表", 0)
+        self.button_update.Disable()
+        t1 = Thread( None, self.UpdateSubjectListSync, "update")
+        t1.start()
+        #t1.setDaemon( True)
+
 class YunxiaoMainFrame( wx.Frame):
     textbox_account = None
     textbox_password = None
@@ -46,6 +113,11 @@ class YunxiaoMainFrame( wx.Frame):
     yunxiao = None
 
     list_courses = None
+
+    courses = None
+    lock_courses = None
+
+    dict_opened_activities = dict()
 
     def __init__( self, parent, title):
         super( YunxiaoMainFrame, self).__init__( parent, title=title, size=( 550, 400), style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
@@ -92,6 +164,7 @@ class YunxiaoMainFrame( wx.Frame):
         #
         self.button_refresh = wx.Button( self, label = "无课或获取失败，点击刷新")
         self.Bind( wx.EVT_BUTTON, self.DidRefreshPress, self.button_refresh)
+        self.button_refresh.Hide()
 
         hbox.Add( label_account, 0, wx.ALL | wx.EXPAND, 5)
         hbox.Add( self.textbox_account, 0, wx.ALL | wx.EXPAND, 5)
@@ -114,24 +187,24 @@ class YunxiaoMainFrame( wx.Frame):
         panel_down.SetSizer( hbox_down)
 
         # FIXME: delete debug code
-        self.AddCourseInfo( "00", "没有选课", "0", "1919/8/10 11:45:14", False, True)
+        self.AddCourseInfo( "00", "没有选课", "f3e5966f-c9af-4721-a87b-83e3e11d66c5", "1919/8/10 11:45:14", False, True)
 
         panel_down.Fit()
         panel2.Fit()
         panel1.Fit()
         self.list_courses.Fit()
 
-        self.Bind( wx.EVT_SIZE, self.DidSizeChange, self)
-
-        self.Center()
-        self.Show()
-    
-    def DidSizeChange( self, event):
         sb = self.GetStatusBar()
         r1 = sb.GetFieldRect( 2)
         p1 = sb.GetPosition()
         r1.x += p1.x; r1.y += p1.y
         self.button_refresh.SetRect( r1)
+
+        self.Center()
+        self.Show()
+
+        # mutex
+        self.lock_courses = threading.Lock()
         
     def DidAboutSelected( self, event):
         YunxiaoAboutFrame( self)
@@ -141,8 +214,8 @@ class YunxiaoMainFrame( wx.Frame):
         self.list_courses.SetItem( index, 1, name)
         self.list_courses.SetItem( index, 2, id)
         self.list_courses.SetItem( index, 3, start)
-        self.list_courses.SetItem( index, 4, "NO" if isbegin else "YES")
-        self.list_courses.SetItem( index, 5, "NO" if isend else "YES")
+        self.list_courses.SetItem( index, 4, "YES" if isbegin else "NO")
+        self.list_courses.SetItem( index, 5, "YES" if isend else "NO")
     
     def LoginSync( self, username, password):
         try:
@@ -162,9 +235,25 @@ class YunxiaoMainFrame( wx.Frame):
     
     def DidRefreshPress( self, event):
         self.UpdateCourseStatus()
+    
+    def DidCloseActivityWindow( self, i):
+        #self.set_opened_activities.remove( i)
+        del self.dict_opened_activities[i]
+        pass
 
     def DidCourseListSelected( self, event):
-        print( event.GetIndex())
+        i = event.GetIndex()
+        if( i in self.dict_opened_activities):
+            this_wnd = self.dict_opened_activities[i]
+            this_wnd.Raise()
+            return
+        # FIXME: use the 1st statement
+        # current_course = self.courses[i]
+        current_course = [ "没有选课", "f3e5966f-c9af-4721-a87b-83e3e11d66c5", 0, False, True]
+
+        wnd = YunxiaoSelectorFrame( self, self.yunxiao, current_course, i)
+        wnd.Show()
+        self.dict_opened_activities[i] = wnd
     
     def DidLoginButtonPressed( self, event):
         self.SetStatusText( "正在登录", 1)
@@ -180,37 +269,46 @@ class YunxiaoMainFrame( wx.Frame):
         self.textbox_password.SetValue( password)
 
         thread_login = Thread( None, self.LoginSync, "login", [ username, password])
-        thread_login.setDaemon( True)
+        #thread_login.setDaemon( True)
         thread_login.start()
     
+    """
+        填充活动列表
+    """
+    def FillCoursesList( self, courses):
+        for i in range( len( courses)):
+            c = courses[i]
+            self.AddCourseInfo(
+                "{0}".format( i),
+                c[0],
+                str( c[1]),
+                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(c[2])),
+                c[3],
+                c[4])
+    
     def GetCoursesSync( self):
+        self.lock_courses.acquire()
         courses = self.yunxiao.GetCourse()
         count = len( courses)
         if count == 0: # 没课或失败
-            pass
+            wx.CallAfter( self.button_refresh.Show)
+            wx.CallAfter( self.SetStatusText, "", 2)
+            wx.CallAfter( self.list_courses.ClearAll)
         else: # 有课
-            pass
+            wx.CallAfter( self.button_refresh.Hide)
+            hint = "可选课程数：{0}，双击课程以打开".format( count)
+            wx.CallAfter( self.SetStatusText, hint, 2)
+            wx.CallAfter( self.list_courses.ClearAll)
+            wx.CallAfter( self.FillCoursesList, self.courses)
+
+        self.lock_courses.release()
 
     def UpdateCourseStatus( self):
-        self.button_refresh.Disable()
-        """ courses = self.yunxiao.GetCourse()
-        count = len( courses)
-        if( count == 0):
-            self.SetStatusText( "暂无可选课程", 2)
-            self.list_courses.ClearAll()
-        else:
-            hint = "可选课程数：{0}，双击课程以打开".format( count)
-            self.SetStatusText( hint, 2)
-            self.list_courses.ClearAll() """
-
-class YunxiaoSelectorFrame( wx.Frame):
-    yunxiao = None
-
-    def __init__( self, parent, yunxiao):
-        super( YunxiaoSelectorFrame, self).__init__( parent, title = "", size = ( 400, 400))
-        self.yunxiao = yunxiao
-
-        self.Show()
+        self.button_refresh.Hide()
+        self.SetStatusText( "正在刷新活动列表", 2)
+        t = Thread( None, self.GetCoursesSync, "get_course")
+        t.start()
+        #t.setDaemon( True)
 
 if __name__ == "__main__":
     app = wx.App()
